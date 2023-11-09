f297598e4957743cb8c0db5ffa8f01e001c3f4b8
Angelica8505 <angelicaaguilar983@gmail.com>
2023-11-09 17:24:54 +0800

    feat/create-table-animals

--- veterinary.sql
+++ veterinary.sql
@@ -9,3 +9,16 @@ phone VARCHAR(100),
 email VARCHAR(100),
 PRIMARY KEY (ownerid)
 );
+CREATE TABLE IF NOT EXISTS public.animals
+(
+animalid INT NOT NULL,
+name VARCHAR(50) NOT NULL,
+species VARCHAR(50) NOT NULL,
+breed VARCHAR(50) NOT NULL,
+dateofbirth DATE NOT NULL,
+gender VARCHAR(10) NOT NULL,
+color VARCHAR(50) NOT NULL,
+ownerid INT NOT NULL,
+PRIMARY KEY(animalid),
+FOREIGN KEY (ownerid) REFERENCES owners(ownerid)
+);
